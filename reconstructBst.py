cb5eee50ec69b07d9a450d17f82bd9ca24617e52
Adewole Caleb Erioluwa <caleberioluwa@gmail.com>
2021-03-12 15:49:42 +0100

    add code to the reconstructBST question

--- reconstructBst.py
+++ reconstructBst.py
@@ -5,7 +5,24 @@ class BST:
         self.left = left
         self.right = right
 
+    def insert(self, val):
+        if val < self.value:
+            if self.left is None:
+                self.left = BST(val)
+            else:
+                self.left.insert(val)
+        else:
+            if self.right is None:
+                self.right = BST(val)
+            else:
+                self.right.insert(val)
+
 
 def reconstructBst(preOrderTraversalValues):
-    # Write your code here.
-    return None
+    if len(preOrderTraversalValues) == 0:
+        return None
+    root = BST(preOrderTraversalValues[0])
+    for idx in range(1, len(preOrderTraversalValues)):
+        val = preOrderTraversalValues[idx]
+        root.insert(val)
+    return root
