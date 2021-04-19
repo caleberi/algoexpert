fc082ccc4633a18da612ba0e7f6f37a4601b70f6
Caleb Adewole <caleberioluwa@gmail.com>
2021-04-19 18:11:32 +0100

    solution to class photo problem

--- classPhotos.py
+++ classPhotos.py
@@ -1,3 +1,17 @@
 def classPhotos(redShirtHeights, blueShirtHeights):
-    # Write your code here.
-    return False
+	tallestRed,shortestRed = getTallestStudentInRow(redShirtHeights)
+	tallestBlue,shortestBlue = getTallestStudentInRow(blueShirtHeights)
+	redAsFront=False
+	if tallestRed>=tallestBlue and shortestRed>=shortestBlue:
+		redAsFront=True
+    redShirtHeights.sort()
+	blueShirtHeights.sort()
+	arrangement= zip(blueShirtHeights,redShirtHeights) if redAsFront else zip(redShirtHeights,blueShirtHeights)
+	for col in arrangement:
+		if col[0]>=col[1]:
+			return False
+    return True
+
+
+def getTallestStudentInRow(row):
+	return (max(row),min(row))
