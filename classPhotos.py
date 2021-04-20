25e00abf8664a685f3d87e135907e2c05f97d8fd
Caleb Adewole <caleberioluwa@gmail.com>
2021-04-20 10:45:42 +0100

    add another solution to the classPhoto.py

--- classPhotos.py
+++ classPhotos.py
@@ -15,3 +15,21 @@ def classPhotos(redShirtHeights, blueShirtHeights):
 
 def getTallestStudentInRow(row):
 	return (max(row),min(row))
+
+
+
+def classPhotos(redShirtHeights, blueShirtHeights):
+    redShirtHeights.sort(reverse=True)
+	blueShirtHeights.sort(reverse=True)
+	shirtColorInFirstRow = 'RED' if redShirtHeights[0]<blueShirtHeights[0] else 'BLUE'
+	for idx in range(len(redShirtHeights)):
+		redShirtHeight = redShirtHeights[idx]
+		blueShirtHeight = blueShirtHeights[idx]
+
+		if shirtColorInFirstRow == "RED":
+			if redShirtHeight>=blueShirtHeight:
+				return False
+		else:
+			if blueShirtHeight>=redShirtHeight:
+				return False
+    return True
